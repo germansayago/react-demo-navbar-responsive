5901b81b831b47fc1517eff5edff03c9bdc9010f
German G Sayago <german.sayago@gmail.com>
2021-01-02 20:47:18 -0300

    navbar responsive ok

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Button } from '../Button';
+import { MenuItems } from './MenuItems';
+
+import './Navbar.css';
+
+class Navbar extends Component {
+  state = {
+    clicked: false
+  };
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    });
+  };
+  render() {
+    return (
+      <nav className='NavbarItems'>
+        <div className='navbar-logo'>
+          React <i className='fab fa-react'></i>
+        </div>
+        <div className='menu-icon' onClick={this.handleClick}>
+          <i
+            className={
+              this.state.clicked ? 'fas fa-fw fa-times' : 'fas fa-fw fa-bars'
+            }
+          ></i>
+        </div>
+        <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <Button>Sign Up</Button>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
